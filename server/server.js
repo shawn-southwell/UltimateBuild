b2c2e7edf790b469f2ab9ab4a892f8f22ac7ec0a
shawn-southwell <shawn.southwel@gmail.com>
2016-09-09 07:24:58 -0700

    started on the express routes, mongoose middleware

--- server/server.js
+++ server/server.js
@@ -4,12 +4,21 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const transpileConfig = require('./controllers/transpile_config_form.js');
-const userController = require('./user/userController');
+const userController = require('./controllers/userController');
 const cookieController = require('./util/cookieController');
-const sessionController = require('./session/sessionController');
+const sessionController = require('./controllers/sessionController');
+
+app.use(express.static('../client/'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', function(req,res){
-
+    res.sendFile(__dirname + '/client/src/index.html')
 })
-
+//app.post to signup should:
+//creater a user
+//setSSIDcookie,
+//start a session
+//send a 200 response to the client
+app.post('/signup', userController.createUser,  )
 app.listen(3000,() => console.log('listening on port 3000'));
