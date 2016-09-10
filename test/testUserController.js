f120ac949cb244c4244f78ededea2e0dc3dd1175
shawn-southwell <shawn.southwel@gmail.com>
2016-09-10 13:46:09 -0700

    trying to resolve configcreate bug

--- test/testUserController.js
+++ test/testUserController.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const expect = require('chai').expect;
+const expect = require('expect');
 const PORT = process.env.PORT || 9090;
 const HOST = `http://localhost:${PORT}`;
 
@@ -14,13 +14,27 @@ describe('Testing user related routes and controllers', () => {
       });
     });
   });
-    describe('/createconfig', () => {
-      it('responds to valid request with 200 status and application/json content type', done => {
-        request(HOST)
-          .get('/creatconfig')
-          .set('accept','text\/html')
-          .expect('Content-Type',"text\/html//; charset=utf-8")
-          .expect(200, done);
+});
+describe('Testing configuration related routes and controllers', () => {
+  describe('/creatconfig', () => {
+  describe('POST', () => {
+    it('responds to valid request with 200 status and application/json content type', done => {
+      const configTemplate = {
+            entry: 'entry',
+            output: 'output',
+            test: 'test',
+            loader: 'loader',
+            exclude: 'exclude',
+            presets: 'presets',
+            refID: 'refID',
+          };
+      request(HOST)
+        .post('/createconfig')
+        .send(configTemplate)
+        .expect('Content-Type', /application\/json/)
+        .expect(200, done);
       });
-    })
+    });
+  });
 })
+  
